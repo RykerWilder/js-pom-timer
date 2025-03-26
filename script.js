997eaa63ec5ed94dfb941d08b6743b536ee3dbe1
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2025-03-26 12:12:19 +0100

    feat: updateDisplay() method

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 const $one = document.querySelector.bind(document);
 const $all = document.querySelectorAll.bind(document);
 
+const pomTimer = $one('#pomodoro-timer');
+const breakTimer = $one('#break-timer');
+
+let flagPom = true;
+let flagBreak = false;
+
+function updateDisplay() {
+    if (flagPom) {
+        pomTimer.classList.remove('hidden');
+        breakTimer.classList.add('hidden');
+    } else {
+        pomTimer.classList.add('hidden');
+        breakTimer.classList.remove('hidden');
+    }
+}
+
+pomTimer.addEventListener('click', () => {
+    if (!flagPom) {
+        flagPom = true;
+        flagBreak = false;
+        updateDisplay();
+    }
+});
+
+breakTimer.addEventListener('click', () => {
+    if (!flagBreak) {
+        flagBreak = true;
+        flagPom = false;
+        updateDisplay();
+    }
+});
+
+// Inizializza il display
+updateDisplay();
